70577293797589ffc9ab53ed123b1dd09cc54d96
Thomas Korsnes <thomaskorsnes@Thomas-sin-MacBook-Air.local>
2025-03-28 12:59:03 +0100

    det funker

--- src/CSVParser.java
+++ src/CSVParser.java
@@ -1,3 +1,22 @@
-public class CSVParser {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class CSVParser {
+    ArrayList<City> listCities = new ArrayList<>();
+
+    public void printFile() {
+        try (Scanner scanner = new Scanner(new File("files/worldcities.csv"))) {
+            System.out.println("yes");
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                City city = new City(scanner.nextLine());
+                listCities.add(city);
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("failed?");
+            throw new RuntimeException(e);
+        }
+    }
 }
