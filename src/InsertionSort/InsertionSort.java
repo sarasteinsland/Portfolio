a8d631a2ce1410f75d49ea2f98f1c12e5e0ba5c8
sarasteinsland <sarasteinsland01@gmail.com>
2025-04-01 12:45:07 +0200

    insertionsort

--- src/InsertionSort/InsertionSort.java
+++ src/InsertionSort/InsertionSort.java
@@ -1,5 +1,23 @@
 package InsertionSort;
 
+import Model.City;
+
 public class InsertionSort {
-    
+
+    public void sort(City[] cities) {
+        int pivot = 1;
+
+        for (int i = pivot; i < cities.length; i++) {
+            for (int x = pivot; x > 0; x--) {
+                if (cities[x].latitude < cities[x - 1].latitude) {
+                    City temp = cities[x];
+                    cities[x] = cities[x - 1];
+                    cities[x - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+            pivot++;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
+import InsertionSort.InsertionSort;
 import Utils.CSVParser;
 import Model.City;
 import BubbleSort.OptimizedBubbleSort;
 import BubbleSort.UnOptimizedBubbleSort;
+import Utils.Test;
+
+import java.util.Arrays;
 
 void main() throws InterruptedException {
     CSVParser parser = new CSVParser();
     City[] worldCities = parser.getCities();
     City[] worldCities2 = Arrays.copyOf(worldCities, worldCities.length);
+    City[] worldCities3 = Arrays.copyOf(worldCities, worldCities.length);
     long start;
     long end;
+    Test test = new Test();
 
     UnOptimizedBubbleSort unOptimizedBubbleSort = new UnOptimizedBubbleSort();
     OptimizedBubbleSort optimizedBubbleSort = new OptimizedBubbleSort();
+    InsertionSort insertionSort = new InsertionSort();
 
+    test.start();
+    /*
+    test.restartTimer();
     start = System.currentTimeMillis();
     System.out.println("Optimized BubbleSort starting...");
     optimizedBubbleSort.sort(worldCities);
@@ -21,9 +31,19 @@ void main() throws InterruptedException {
 
     Thread.sleep(2000);
 
+    test.restartTimer();
     start = System.currentTimeMillis();
     System.out.println("Unoptimized BubbleSort starting...");
     unOptimizedBubbleSort.sort(worldCities2);
     end = System.currentTimeMillis();
     System.out.printf("UnoptimizedBubbleSort sorting time: %d ms\n", (end - start));
+
+    Thread.sleep(2000);
+*/
+    start = System.currentTimeMillis();
+    System.out.println("Insertion sort starting...");
+    insertionSort.sort(worldCities3);
+    end = System.currentTimeMillis();
+    System.out.printf("Insertion sort sorting time: %d ms\n", (end - start));
+    test.stop = true;
 }
